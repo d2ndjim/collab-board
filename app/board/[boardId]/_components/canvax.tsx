8be34af2916093a8282df8f91cd8878a10c47088
Lekan <jimohridwanlekan@gmail.com>
2024-05-14 20:50:27 +0100

    Add other toolbar functionality

--- app/board/[boardId]/_components/canvax.tsx
+++ app/board/[boardId]/_components/canvax.tsx
@@ -28,6 +28,7 @@ import { useCallback, useMemo, useState } from "react";
 import { CursorsPrescence } from "./cursors-prescence";
 import {
   connectionIdToColor,
+  findIntersectingLayersWithRectangle,
   pointerEventToCanvasPoint,
   resizeBounds,
 } from "@/lib/utils";
@@ -124,6 +125,21 @@ export const Canvax = ({ boardId }: CanvaxProps) => {
     }
   }, []);
 
+  const updateSelectionNet = useMutation(({ storage, setMyPresence }, current: Point, origin: Point) => {
+    const layers = storage.get("layers").toImmutable()
+    setCanvasState({ mode: CanvasMode.SelectionNet, origin, current });
+
+    const ids = findIntersectingLayersWithRectangle(layerIds, layers, origin, current);
+    
+    setMyPresence({ selection: ids });
+  }, [layerIds])
+
+  const startMultiSelection = useCallback((current: Point, origin: Point) => {
+    if (Math.abs(current.x - origin.x) + Math.abs(current.y - origin.y) > 5) {
+      setCanvasState({ mode: CanvasMode.SelectionNet, origin, current });
+    }
+  }, []);
+
   const resizeSelectedLayer = useMutation(
     ({ storage, self }, point: Point) => {
       if (canvasState.mode !== CanvasMode.Resizing) {
@@ -171,7 +187,11 @@ export const Canvax = ({ boardId }: CanvaxProps) => {
 
       const current = pointerEventToCanvasPoint(e, camera);
 
-      if (canvasState.mode === CanvasMode.Translating) {
+      if (canvasState.mode === CanvasMode.Pressing) {
+        startMultiSelection(current, canvasState.origin);
+      } else if (canvasState.mode === CanvasMode.SelectionNet) {
+        updateSelectionNet(current, canvasState.origin);
+      } else if (canvasState.mode === CanvasMode.Translating) {
         translateSelectedLayers(current);
       } else if (canvasState.mode === CanvasMode.Resizing) {
         resizeSelectedLayer(current);
@@ -293,6 +313,15 @@ export const Canvax = ({ boardId }: CanvaxProps) => {
             />
           ))}
           <SelectionBox onResizeHandlePointerDown={onResizeHandlePointerDown} />
+          {canvasState.mode === CanvasMode.SelectionNet && canvasState.current != null && (
+            <rect 
+              className="fill-blue-500 stroke-blue-500 stroke-1"
+              x={Math.min(canvasState.origin.x, canvasState.current.x)}
+              y={Math.min(canvasState.origin.y, canvasState.current.y)}
+              width={Math.abs(canvasState.origin.x - canvasState.current.x)}
+              height={Math.abs(canvasState.origin.y - canvasState.current.y)}
+            />
+          )}
           <CursorsPrescence />
         </g>
       </svg>
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,4 @@
-import { Camera, Color, Point, Side, XYWH } from "@/types/canvas";
+import { Camera, Color, Layer, Point, Side, XYWH } from "@/types/canvas";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
@@ -32,7 +32,7 @@ export function resizeBounds(bounds: XYWH, corner: Side, point: Point) {
     y: bounds.y,
     width: bounds.width,
     height: bounds.height,
-  }
+  };
 
   if ((corner & Side.Left) === Side.Left) {
     result.x = Math.min(point.x, bounds.x + bounds.width);
@@ -55,5 +55,47 @@ export function resizeBounds(bounds: XYWH, corner: Side, point: Point) {
   }
 
   return result;
+}
 
+export function findIntersectingLayersWithRectangle(
+  layerIds: readonly string[],
+  layers: ReadonlyMap<string, Layer>,
+  a: Point,
+  b: Point,
+) {
+  const rect = {
+    x: Math.min(a.x, b.x),
+    y: Math.min(a.y, b.y),
+    width: Math.abs(a.x - b.x),
+    height: Math.abs(a.y - b.y),
+  };
+
+  const ids = [];
+
+  for (const layerId of layerIds) {
+    const layer = layers.get(layerId);
+
+    if (layer == null) {
+      continue;
+    }
+
+    const { x, y, height, width } = layer;
+
+    if (
+      rect.x + rect.width > x &&
+      rect.x < x + width &&
+      rect.y + rect.height > y &&
+      rect.y < y + height
+    ) {
+      ids.push(layerId);
+    }
+  }
+
+  return ids;
+}
+
+export function getContrastingTextColor(color: Color) {
+  const luminance = 0.299 * color.r + 0.587 * color.g + 0.114 * color.b;
+
+  return luminance > 182 ? "black" : "white";
 }
